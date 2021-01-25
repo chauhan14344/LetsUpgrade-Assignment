cafd97eccef95530c01d728a8726c589aa7d7e10
chauhan14344 <77699551+chauhan14344@users.noreply.github.com>
2021-01-25 16:44:34 +0530

    Assignment 2

---
+++ Assignment 3/Empty_list.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[10]:
+
+
+# Python program to remove empty List from List
+sample_list = ['ABC',123, [], "xyz", []] 
+new_list = [ele for ele in sample_list if ele != []] 
+print ("List after removing empty list : " + str(new_list)) 
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
